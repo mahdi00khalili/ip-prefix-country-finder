9d001198d83187a09b53bf9d744e44030706dbfe
nistootsin <nistootsin@gmail.com>
2024-11-12 20:07:56 +0330

    remove update country from menu items

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ def quit_program():
 
 while True:
     operation = input(
-        '1. init the project \n2. update countries\n3. update ip-country table\n4. finding the list of countries for input perfix\n0. quit\n\nSelect an operation: >>> ')
+        '1. init the project \n2. update ip-country table\n3. finding the list of countries for input perfix\n0. quit\n\nSelect an operation: >>> ')
     if operation == '1':
         # Run main.py
         subprocess.run(["python", "main_mudules/main.py"])
@@ -23,16 +23,11 @@ while True:
         subprocess.run(["python3", "main_mudules/add_ip_perfix.py"])
         print('Done\n')
     elif operation == '2':
-        # Run add_update_countries.py
-        print('updating the country table.')
-        subprocess.run(["python3", "main_mudules/add_update_countries.py"])
-        print('Done\n')
-    elif operation == '3':
         # Run add_update_ip_country.py
         print('updating the ip_country table.')
         subprocess.run(["python3", "main_mudules/add_update_ip_country.py"])
         print('Done\n')
-    elif operation == '4':
+    elif operation == '3':
         # Run finding_countries.py
         while True:
             subprocess.run(["python3", "main_mudules/finding_countries.py"])
